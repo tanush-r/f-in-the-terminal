577ce16f9c8385051560a0aa4b95bf9cc82fd45e
Tanush R <tanushmal@gmail.com>
2022-01-20 20:25:08 +0530

    first commit

---
+++ bruhhh.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+
+print("f in the terminal")
+x = "f"
+while x == "f" :
+    x = input()
+print("Reporting crime..")
+sleep(5)
+print("Your crime has been reported to the FBI")
